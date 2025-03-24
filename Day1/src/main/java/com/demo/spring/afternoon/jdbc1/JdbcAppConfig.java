439e2d304aa341a4e6cd621b5c190febac6cdeee
GowthamanM9 <gowthaman999@gmail.com>
2025-03-24 17:34:29 +0530

    Day wise folder rearrange

---
+++ Day1/src/main/java/com/demo/spring/afternoon/jdbc1/JdbcAppConfig.java
@@ -0,0 +1,34 @@
+package com.demo.spring.day1.afternoon.jdbc1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.demo.spring.day1.afternoon.jdbc")
+public class JdbcAppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/libraryDb");
+        ds.setUsername("root");
+        ;
+        ds.setPassword("P@ssword");
+
+        return ds;
+    }
+
+    public JdbcTemplate jdbcTemplate(DataSource ds) {
+        JdbcTemplate jt = new JdbcTemplate(ds);
+
+        return jt;
+    }
+
+
+}
---
+++ Day1/src/main/java/com/demo/spring/afternoon/jdbc1/JdbcMain.java
@@ -0,0 +1,7 @@
+package com.demo.spring.day1.afternoon.jdbc1;
+
+public class JdbcMain {
+    public static void main(String[] args) {
+
+    }
+}
