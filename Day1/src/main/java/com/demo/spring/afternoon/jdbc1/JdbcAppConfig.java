7dc36e11a5641c830cafe15354367c54d37491c7
GowthamanM9 <gowthaman999@gmail.com>
2025-03-24 17:59:26 +0530

    JDBC 1 done

--- Day1/src/main/java/com/demo/spring/afternoon/jdbc1/JdbcAppConfig.java
+++ Day1/src/main/java/com/demo/spring/afternoon/jdbc1/JdbcAppConfig.java
@@ -1,9 +1,10 @@
-package com.demo.spring.day1.afternoon.jdbc1;
+package com.demo.spring.afternoon.jdbc1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -18,17 +19,20 @@ public class JdbcAppConfig {
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/libraryDb");
         ds.setUsername("root");
-        ;
         ds.setPassword("P@ssword");
 
         return ds;
     }
 
+    @Bean
     public JdbcTemplate jdbcTemplate(DataSource ds) {
         JdbcTemplate jt = new JdbcTemplate(ds);
-
         return jt;
     }
 
-
+    @Bean
+    public JdbcClient jdbcClient(DataSource ds) {
+        JdbcClient client = JdbcClient.create(ds);
+        return client;
+    }
 }
--- Day1/src/main/java/com/demo/spring/afternoon/jdbc1/JdbcMain.java
+++ Day1/src/main/java/com/demo/spring/afternoon/jdbc1/JdbcMain.java
@@ -1,7 +1,45 @@
-package com.demo.spring.day1.afternoon.jdbc1;
+package com.demo.spring.afternoon.jdbc1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 public class JdbcMain {
     public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(JdbcAppConfig.class);
 
+        // Template pattern
+        JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+        jdbcTemplate.query(new PreparedStatementCreator() {
+
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pst = con.prepareStatement("select * from book");
+                return pst;
+            }
+        }, new RowCallbackHandler() {
+
+            @Override
+            public void processRow(ResultSet rs) throws SQLException {
+                System.out.println(rs.getString("isbn") + " " + rs.getString("name"));
+            }
+        });
+
+        // JDBC Client pattern
+        JdbcClient jdbcClient = ctx.getBean("jdbcClient", JdbcClient.class);
+        List<Map<String, Object>> data = jdbcClient.sql("select * from book").query().listOfRows();
+        for (Map map : data) {
+            System.out.println(map);
+        }
     }
 }
